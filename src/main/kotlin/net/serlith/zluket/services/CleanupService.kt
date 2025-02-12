7a8cfdadadfa88715aa946478a256324356b14ec
Schroddinger <zxctl@proton.me>
2025-02-11 21:16:37 -0500

    Added some customizations to application.properties

--- src/main/kotlin/net/serlith/zluket/services/CleanupService.kt
+++ src/main/kotlin/net/serlith/zluket/services/CleanupService.kt
@@ -2,6 +2,7 @@ package net.serlith.zluket.services
 
 import net.serlith.zluket.databases.PasteRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -14,9 +15,12 @@ class CleanupService
     private val pasteRepository: PasteRepository,
 ) {
 
+    @Value("\${zluket.cleanup.weeks}")
+    private var cleanupWeeks: Long = 1L
+
     @Scheduled(fixedRate = 1, timeUnit = TimeUnit.HOURS)
     fun performCleanup() {
-        this.pasteRepository.findAllByCreatedBefore(LocalDateTime.now().minusWeeks(1))
+        this.pasteRepository.findAllByCreatedBefore(LocalDateTime.now().minusWeeks(this.cleanupWeeks))
     }
 
 }
